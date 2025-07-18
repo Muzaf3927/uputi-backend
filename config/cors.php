d7b91a5d869da1e036ee804cd8e9849d16a29549
Muzaffar <khamroevmuzaf@gmail.com>
2025-07-18 14:55:07 +0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins' => ['http://192.168.1.234:3000'], // ← IP твоего фронта
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
-]; 
+
+    'supports_credentials' => true, // ← ОБЯЗАТЕЛЬНО для Sanctum
+];
