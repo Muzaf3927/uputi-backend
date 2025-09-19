2f84892b79a179807b10a1aa325e9889bff13141
Muzaffar <khamroevmuzaf@gmail.com>
2025-09-19 17:40:15 +0500

    seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Http\JsonResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Response::macro('json', function ($data = [], $status = 200, array $headers = [], $options = 0) {
+            // всегда добавляем глобальные опции из config/app.php
             $options = $options | config('app.json_encode_options', 0);
-            return response()->json($data, $status, $headers, $options);
+
+            return new JsonResponse($data, $status, $headers, $options);
         });
     }
 }
