2619e730f4f640cc1e3d4175ab1e3d061a29e3bb
Muzaffar <khamroevmuzaf@gmail.com>
2025-10-07 11:10:34 +0500

    soft delete users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Muzaf',
-            'phone' => '900012314',
+            'name' => 'Test User',
+            'phone' => '900000000',
             'role' => 'driver',
-            'password' => Hash::make('900012314'),
+            'password' => Hash::make('900000000'),
             'balance' => 10000,
             'rating' => 5.0,
             'rating_count' => 25,
