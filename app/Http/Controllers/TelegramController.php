c2ea154a88a11bc6e04049444413222304b017ee
Muzaffar <khamroevmuzaf@gmail.com>
2025-11-09 14:22:55 +0500

    telegramBot2

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,31 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Telegram\Bot\Api;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 
 class TelegramController extends Controller
 {
     public function webhook(Request $request)
     {
-        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
-        $update = $telegram->getWebhookUpdate();
+        $update = $request->all();
+        Log::info('tg update', $update);
 
-        $message = $update->getMessage();
+        $message = $update['message'] ?? null;
+
+        if (!$message) {
+            return response('ok', 200);
+        }
+
         $chatId = $message['chat']['id'] ?? null;
         $text = $message['text'] ?? '';
 
         if ($text === '/start') {
+
             $keyboard = [
                 [
-                    ['text' => 'Открыть приложение 🚗', 'web_app' => ['url' => 'https://www.uputi.net']]
+                    [
+                        'text' => 'UPuti programmasini ochish 🚗',
+                        'web_app' => ['url' => 'https://www.uputi.net']
+                    ]
                 ]
             ];
 
-            $telegram->sendMessage([
+            Http::post("https://api.telegram.org/bot".env('TELEGRAM_BOT_TOKEN')."/sendMessage", [
                 'chat_id' => $chatId,
-                'text' => "Добро пожаловать в UPuti! Нажми кнопку ниже 👇",
-                'reply_markup' => json_encode(['keyboard' => $keyboard, 'resize_keyboard' => true])
+                'text' => "UPuti ga xush kelibsiz! UPuti programmasini ochish uchun pastdagi tugmaga bosing",
+                'reply_markup' => json_encode([
+                    'keyboard' => $keyboard,
+                    'resize_keyboard' => true
+                ]),
             ]);
         }
 
